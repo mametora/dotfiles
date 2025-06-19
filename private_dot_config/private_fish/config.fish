a9de879d6704e9f8fe78621f5cf5216ab39922e8
Takanori Masuda <mametora@mmtr.me>
2025-06-19 13:22:34 +0900

    Initial commit

---
+++ private_dot_config/private_fish/config.fish
@@ -0,0 +1,31 @@
+set -Ux VISUAL "code --wait --new-window"
+set -Ux EDITOR "code --wait --new-window"
+eval (/opt/homebrew/bin/brew shellenv)
+
+# ASDF configuration code
+if test -z $ASDF_DATA_DIR
+    set _asdf_shims "$HOME/.asdf/shims"
+else
+    set _asdf_shims "$ASDF_DATA_DIR/shims"
+end
+
+# Do not use fish_add_path (added in Fish 3.2) because it
+# potentially changes the order of items in PATH
+if not contains $_asdf_shims $PATH
+    set -gx --prepend PATH $_asdf_shims
+end
+set --erase _asdf_shims
+
+# pnpm
+set -gx PNPM_HOME "/Users/masuda/Library/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+  set -gx PATH "$PNPM_HOME" $PATH
+end
+# pnpm end
+
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+# Added by LM Studio CLI (lms)
+set -gx PATH $PATH /Users/masuda/.lmstudio/bin
